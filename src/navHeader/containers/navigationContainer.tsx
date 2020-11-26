8f0952a4d9f5d21877a17bfb79c7873ff2580b35
paolomajerano <paolo.majerano@ucalgary.ca>
2020-11-26 14:57:59 -0700

    init

---
+++ src/navHeader/containers/navigationContainer.tsx
@@ -0,0 +1,10 @@
+import React ,{ Component } from "react";
+import { Button } from "@material-ui/core";
+
+class NavPage extends Component {
+  render() {
+    return <Button>hello</Button>;
+  }
+}
+
+export default NavPage;
